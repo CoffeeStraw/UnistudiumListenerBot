30cd5d35de2080a31c8943eb868bc1b4f99e7677
Porchetta <clarantonio98@gmail.com>
2018-02-14 13:11:21 +0100

    [UPDATE] Aggiunta creazione file pid, migliorata resa dell'output, ottimizzato il codice

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ Author: Porchetta (clarantonio98@gmail.com)
 TO DO:
 - Commit iniziale contenente info e comandi base                                [V]
 - Comando per effettuare un tentativo di connessione con le credenziali         [V]
-- Comando per aggiungere tra i corsi di studio seguiti quelli desiderati        [ ]
-- Comando per rimuovere  tra i corsi di studio seguiti quelli desiderati        [ ]
+- Comando per aggiungere tra i corsi di studio seguiti quelli desiderati        [V]
+- Comando per rimuovere  tra i corsi di studio seguiti quelli desiderati        [V]
     - Invio di un messaggio contenente il nuovo aggiornamento                   [ ]
-- Comando per abilitare/disabilitare le notifiche                               [ ]
 '''
 #!/usr/bin/python3.6
 import os
+import sys
 import time
 import json
 import pickle
@@ -41,7 +41,7 @@ def handle(msg):
     if content_type == 'text':
         cmd_input = msg['text']
     else:
-        bot.sendMessage(chat_id, "Il messaggio che hai inviato non è valido, ritenta.")
+        bot.sendMessage(chat_id, "Il messaggio che hai inviato non è valido, ritenta")
 
     # Attempting to save username and full name
     try:
@@ -51,12 +51,13 @@ def handle(msg):
         username  = "Not defined"
         full_name = "Not defined"
 
+    # Prints msg from the user
     print("Msg from {}@{}{}[{}]: \t\"{}{}{}\"".format(color.BOLD, username, color.END, str(chat_id), color.ITALIC, cmd_input, color.END))
 
     if basics_cmds_response(chat_id, cmd_input) != 0:
-        print("Ho eseguito un comando base")
+        pass
     elif cmd_input == "/attempt_login" or cmd_input == "/attempt_login"+bot_name:
-        print(color.CYAN + "Tentativo di connessione con |" + cred_get("username") + " - ******** |" + color.END)
+        print(color.CYAN + "[CONNECTION] Tentativo di connessione con |" + cred_get("username") + " - ********|" + color.END)
 
         rec_response = reconnect(chat_id)
         if rec_response == 1:
@@ -64,19 +65,17 @@ def handle(msg):
             pattern = "<span class=\"usertext\">(.+?)</span>"
             name = re.findall(pattern, str(main_page.content))[0]
             bot.sendMessage(chat_id, "Sono riuscito a collegarmi, benvenuto *" + name + "!*", parse_mode = "Markdown")
-
         elif rec_response == 2:
-            bot.sendMessage(chat_id, "Sei già connesso al portale.", parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
-
+            bot.sendMessage(chat_id, "Sei già connesso al portale", parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
         else:
-            bot.sendMessage(chat_id, "*Errore* in fase di *login*, ritenta sostituendo le credenziali nel file _cred.json_.", parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
+            bot.sendMessage(chat_id, "*Errore* in fase di *login*, ritenta sostituendo le credenziali nel file _creSei già connessod.json_", parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
 
 
     elif cmd_input == "/listen" or cmd_input == "/listen"+bot_name:
         courses_names = []
         if not os.path.isfile(fileDir + "full_courses.txt"):
             if reconnect(chat_id):
-                print(color.CYAN + "Creo il file dei corsi partendo dalla lista su UNISTUDIUM." + color.END)
+                print(color.CYAN + "[FILE CREATE] Aggiunto file full_courses.txt" + color.END)
                 main_page = r.get(MAIN_URL)
 
                 pattern = "<h3 class=\"coursename\">(.+?)</h3>"
@@ -94,14 +93,12 @@ def handle(msg):
                 for course in courses:
                     courses_names.append(course[0])
         else:
-            print(color.CYAN + "Il file dei corsi è presente, non mi collego ad UNISTUDIUM." + color.END)
             courses_names = load_courses_names(fileDir + "full_courses.txt")
 
         # List courses
         keyboard_courses = []
-        for i, course_name in enumerate(courses_names):
-            keyboard_courses.append([])
-            keyboard_courses[i].append(course_name)
+        for course_name in courses_names:
+            keyboard_courses.append([course_name])
 
         markup = ReplyKeyboardMarkup(keyboard=keyboard_courses)
         bot.sendMessage(chat_id, "Seleziona il corso che vuoi che ascolti per te", parse_mode = "Markdown", reply_markup = markup)
@@ -112,7 +109,7 @@ def handle(msg):
         if cmd_input in courses_names:
             courses_followed = []
             if not os.path.isfile(fileDir + "courses_followed.txt"):
-                print(color.CYAN + "Aggiungo il file dei corsi seguiti." + color.END)
+                print(color.CYAN + "[FILE CREATE] Aggiunto file courses_followed.txt" + color.END)
 
                 courses_followed.append(cmd_input)
                 with open(fileDir + "courses_followed.txt", 'wb') as f:
@@ -120,8 +117,6 @@ def handle(msg):
 
                 bot.sendMessage(chat_id, "🔔 Da ora in poi riceverai le notifiche di:\n\n*" + cmd_input + "* 🔔", parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
             else:
-                print(color.CYAN + "Non ricreo il file dei corsi seguiti." + color.END)
-
                 with open (fileDir + "courses_followed.txt", 'rb') as f:
                     courses_followed = pickle.load(f)
 
@@ -136,7 +131,7 @@ def handle(msg):
 
             user_state[chat_id] = 0
         else:
-            bot.sendMessage(chat_id, "Invia un corso valido utilizzando la tastiera creata.")
+            bot.sendMessage(chat_id, "Il corso scritto non è presente tra quelli trovati, riprova")
 
     elif cmd_input == "/stop_listen" or cmd_input == "/stop_listen"+bot_name:
         if not os.path.isfile(fileDir + "courses_followed.txt"):
@@ -145,9 +140,8 @@ def handle(msg):
             courses_followed = load_courses_followed(fileDir + "courses_followed.txt")
 
             keyboard_courses = []
-            for i, course_followed in enumerate(courses_followed):
-                keyboard_courses.append([])
-                keyboard_courses[i].append(course_followed)
+            for course_followed in courses_followed:
+                keyboard_courses.append([course_followed])
 
             markup = ReplyKeyboardMarkup(keyboard=keyboard_courses)
             bot.sendMessage(chat_id, "Seleziona il corso che vuoi che smetta di ascoltare", parse_mode = "Markdown", reply_markup = markup)
@@ -162,6 +156,7 @@ def handle(msg):
                 with open(fileDir + "courses_followed.txt", 'wb') as f:
                     pickle.dump(courses_followed, f)
             else:
+                print(color.CYAN + "[FILE DELETE] Rimosso file courses_followed.txt" + color.END)
                 os.remove(fileDir + "courses_followed.txt")
 
             bot.sendMessage(chat_id, "🔕 Da ora in poi non riceverai più notifiche da:\n\n*" + cmd_input + "* 🔕", parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
@@ -176,6 +171,8 @@ def handle(msg):
 # Tries to connect to the unistudium website
 def reconnect(chat_id):
     # Try to ping the server
+    bot.sendChatAction(chat_id, "typing")
+
     response = os.system("ping -c 1 www.unistudium.unipg.it > /dev/null")
     if response:
         if (cred_get("username") != "YOUR_USERNAME" and cred_get("password") != "YOUR_PASSWORD"):
@@ -195,38 +192,20 @@ def reconnect(chat_id):
                 main_cont = str(r.get(MAIN_URL).content)
 
                 if("loginpanel" in main_cont):
-                    print(color.RED + "Credenziali errate" + color.END)
+                    print(color.RED + "[CONNECTION] Credenziali errate" + color.END)
                 else:
-                    print(color.GREEN + "Connesso al portale UNISTUDIUM" + color.END)
+                    print(color.GREEN + "[CONNECTION] Connessione al portale UNISTUDIUM effettuata con successo" + color.END)
                     return 1
             else:
-                print(color.YELLOW + "Sei già connesso" + color.END)
+                print(color.YELLOW + "[CONNECTION] Connessione già instaurata" + color.END)
                 return 2
         else:
-            print(color.RED + "Credenziali non settate" + color.END)
+            print(color.RED + "[CONNECTION] Credenziali non settate" + color.END)
             bot.sendMessage(chat_id, "Non hai inserito il tuo username e/o la tua password nel file _cred.json_. Modificali e riprova.", parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
-        return 0
     else:
-        print(color.RED + "Server irraggiungibile" + color.END)
+        print(color.RED + "[CONNECTION] Server irraggiungibile" + color.END)
         bot.sendMessage(chat_id, "Non riesco a contattare il server, riprova più tardi", parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
-
-#fileDir + "full_courses.txt"
-def load_courses_names(path):
-    courses, courses_names = [], []
-    with open (path, 'rb') as f:
-        courses = pickle.load(f)
-
-    for course in courses:
-        courses_names.append(course[0])
-
-    return courses_names
-
-def load_courses_followed(path):
-    courses_followed = []
-    with open (path, 'rb') as f:
-        courses_followed = pickle.load(f)
-
-    return courses_followed
+    return 0
 
 # Managing callback query from callback buttons in inline keyboards
 def on_callback_query(msg):
@@ -237,7 +216,6 @@ def on_callback_query(msg):
 
 # Standard commands input, with texts imported from settings.py file
 def basics_cmds_response(chat_id, cmd_input):
-
     if cmd_input == "/start" or cmd_input == "/start"+bot_name:
         bot.sendMessage(chat_id, start_msg, parse_mode = "Markdown", reply_markup = ReplyKeyboardRemove(remove_keyboard = True))
         return 1
@@ -248,13 +226,40 @@ def basics_cmds_response(chat_id, cmd_input):
 
     elif cmd_input == "/info" or cmd_input == "/info"+bot_name:
         keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                    [dict(text = 'Dona', url = 'https://placeholder.it'), dict(text = 'GitHub', url = 'https://github.com')]])
+                    [dict(text = 'Dona', url = 'https://google.it'), dict(text = 'GitHub', url = 'https://github.com/Porchetta/UnistudiumListenerBot')]])
         bot.sendMessage(chat_id, info_msg,  parse_mode = "Markdown", reply_markup = keyboard)
         return 1
 
     else:
         return 0
 
+def load_courses(path):
+    course = []
+    with open (path, 'rb') as f:
+        courses = pickle.load(f)
+
+    return courses
+
+def load_courses_names(path):
+    courses_names = []
+    for course in load_courses(path):
+        courses_names.append(course[0])
+
+    return courses_names
+
+def load_courses_urls(path):
+    courses_urls = []
+    for course in load_courses(path):
+        courses_urls.append(course[1])
+
+    return courses_urls
+
+def load_courses_followed(path):
+    courses_followed = []
+    with open (path, 'rb') as f:
+        courses_followed = pickle.load(f)
+
+    return courses_followed
 
 def cred_get(field):
     data = json.load(open('cred.json'))
@@ -263,17 +268,35 @@ def cred_get(field):
 # ----------------
 # Start working...
 # ----------------
-bot = telepot.Bot(TOKEN)
+pid = str(os.getpid())
 
-updates = bot.getUpdates()
-if updates:
-    last_update_id = updates[-1]['update_id']
-    bot.getUpdates(offset=last_update_id+1)
+# Check if PID exist
+if os.path.isfile(pidfile):
+    print(("{}[EXIT] {} Il processo è ancora attivo{}").format(color.RED, pidfile, color.END))
+    sys.exit()
+else:
+    f = open(pidfile, 'w')
+    f.write(pid)
 
-bot.message_loop({'chat': handle,
-                  'callback_query': on_callback_query})
+# Check if fileDir exists
+if not os.path.exists(fileDir):
+    os.makedirs(fileDir)
+    print(color.DARKCYAN + "[FOLDER CREATE] Ho aggiunto la cartella /Files/" + color.END)
 
-print(color.ITALIC + 'Da grandi poteri derivano grandi responsabilità...\n' + color.END)
+try:
+    bot = telepot.Bot(TOKEN)
 
-while(1):
-    time.sleep(10)
+    updates = bot.getUpdates()
+    if updates:
+        last_update_id = updates[-1]['update_id']
+        bot.getUpdates(offset=last_update_id+1)
+
+    bot.message_loop({'chat': handle,
+                      'callback_query': on_callback_query})
+
+    print(color.ITALIC + 'Da grandi poteri derivano grandi responsabilità...\n' + color.END)
+
+    while(1):
+        time.sleep(10)
+finally:
+    os.unlink(pidfile)
